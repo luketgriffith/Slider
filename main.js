17bdab73ad5e1ce2d9b0e58c653b815dc1bcd9ed
Luke Griffith <luketgriffith@gmail.com>
2015-10-06 16:28:17 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var para = $('.meow');
+para.on('click', function(){
+   $('.para').toggleClass('big').addClass('para');
+});
+
+
+var para = $('.meow2');
+para.on('click', function(){
+  $('.para2').toggleClass('big').addClass('para');
+});
+
+
+var para = $('.meow3');
+para.on('click', function(){
+  $('.para3').toggleClass('big').addClass('para');
+});
